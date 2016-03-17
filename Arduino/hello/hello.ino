826cdf61304da0fd7b12bf0a6450639627158e50
chenyiming <1985614@qq.com>
2016-03-17 16:43:01 +0800

    编写一个简单的闪烁程序

--- Arduino/hello/hello.ino
+++ Arduino/hello/hello.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
-
+void setup()
+{
+  pinMode( 1 , OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  digitalWrite( 1 , HIGH );
+  delay( 1000 );
+  digitalWrite( 1 , LOW );
+  delay( 1000 );
 }
+
+
